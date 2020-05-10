12e8e48354d77219fe6be58fe5f260dfa2b5d159
Alexey Yasinsky <alex_yasinsky@mail.ru>
2020-05-10 17:38:09 +0300

    lesson 4 answer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
 'use strict';
+//Задание 1
+
+let task1 = document.getElementById('task1__p');
+let task1Text = task1.innerHTML;
+document.getElementById('task1__btn').addEventListener('click', () => {
+  task1.innerHTML = '';
+  task1.innerHTML = task1Text.replace(/'/g,"\"");
+});
+
+//Задание 2
+
+let task2 = document.getElementById('task2__p');
+let task2Text = task2.innerHTML;
+document.getElementById('task2__btn').addEventListener('click', () => {
+  task2.innerHTML = '';
+  task2Text = task2Text.replace(/\s'/g," \"");
+  task2Text = task2Text.replace(/'<br>/g,"\"<br>");
+  task2.innerHTML = task2Text;
+});
+
+//Задание 3
+
+let form = document.forms[0];
+
+form.addEventListener('submit', () => {
+  event.preventDefault();
+
+  form.name.nextSibling.classList.remove('visible');
+  if (form.name.value.match(/[а-яёa-z ]/gi) != null) {
+    if (form.name.value.match(/[а-яё ]/gi).length != form.name.value.length) {
+      form.name.nextSibling.classList.add('visible');
+    } 
+  } else form.name.nextSibling.classList.add('visible');
+  
+  form.tel.nextSibling.classList.remove('visible');
+  if (form.tel.value.length != 15) {
+    form.tel.nextSibling.classList.add('visible');
+  } else if (!/\+7\(\d{3}\)\d{3}-\d{4}/gi.test(form.tel.value)) {
+    form.tel.nextSibling.classList.add('visible');
+  };
+ 
+  form.email.nextSibling.classList.remove('visible');
+  if (!/\w+@mail.ru|\w+\.\w+@mail.ru|\w+-\w+@mail.ru/.test(form.email.value)) {
+    form.email.nextSibling.classList.add('visible');
+  };
+
+});
+
