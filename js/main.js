793195fbb7123633b513f8c8f84226353bd9476c
Alexey Yasinsky <alex_yasinsky@mail.ru>
2020-05-10 17:55:34 +0300

    lesson 4 answer

--- js/main.js
+++ js/main.js
@@ -27,22 +27,33 @@ form.addEventListener('submit', () => {
   event.preventDefault();
 
   form.name.nextSibling.classList.remove('visible');
+  form.name.classList.remove('red');
   if (form.name.value.match(/[а-яёa-z ]/gi) != null) {
     if (form.name.value.match(/[а-яё ]/gi).length != form.name.value.length) {
       form.name.nextSibling.classList.add('visible');
+      form.name.classList.add('red');
     } 
-  } else form.name.nextSibling.classList.add('visible');
+  } else {
+    form.name.nextSibling.classList.add('visible');
+    form.name.classList.add('red');
+  }
+
   
   form.tel.nextSibling.classList.remove('visible');
+  form.tel.classList.remove('red');
   if (form.tel.value.length != 15) {
     form.tel.nextSibling.classList.add('visible');
+    form.tel.classList.add('red');
   } else if (!/\+7\(\d{3}\)\d{3}-\d{4}/gi.test(form.tel.value)) {
     form.tel.nextSibling.classList.add('visible');
+    form.tel.classList.add('red');
   };
  
   form.email.nextSibling.classList.remove('visible');
+  form.email.classList.remove('red');
   if (!/\w+@mail.ru|\w+\.\w+@mail.ru|\w+-\w+@mail.ru/.test(form.email.value)) {
     form.email.nextSibling.classList.add('visible');
+    form.email.classList.add('red');
   };
 
 });
