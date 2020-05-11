3f28aba0e86f22698678ce89e9ba93d454e4e59c
Alexey Yasinsky <alex_yasinsky@mail.ru>
2020-05-11 08:29:30 +0300

    lesson 5 task

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderProduct = (title, price) => {
-  return `<div class="product-item">
-            <h3>${title}</h3>
-            <p>${price}</p>
-            <button class="by-btn">Добавить в корзину</button>
-          </div>`;
-};
-
-const renderProducts = (list) => {
-  // const productList = list.map((good) => {
-  //   return renderProduct(good.title, good.price);
-  // });
-  const productList = [];
-  list.forEach(good => {
-    productList.push(renderProduct(good.title, good.price));
-  });
-  console.log(productList);
-  document.querySelector('.products').innerHTML = productList;
-};
-
-renderProducts(products);
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        imgCatalog: 'https://placehold.it/200x150'
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product){
+            console.log(product.id_product);
+        }
+    },
+    mounted(){
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                }
+            });
+    }
+});
